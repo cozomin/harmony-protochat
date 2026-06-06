57e20ac895af6ded87bedefad9cbced32fe69162
cozomin <cherciu.cosmin.alexandru@gmail.com>
2026-06-06 19:17:51 +0300

    Added the config for the db in an env file

--- src/main/java/harmony/proto/database/db_config.java
+++ src/main/java/harmony/proto/database/db_config.java
@@ -4,15 +4,16 @@ public record db_config(String jdbcUrl, String username, String password, int ma
 
     public static db_config fromEnv() {
         return new db_config(
-                getenv("DB_URL", "jdbc:postgresql://localhost:5432/harmony"),
-                getenv("DB_USER", "postgres"),
-                getenv("DB_PASSWORD", "SQLpa55"),
+                getenv("DB_URL", "fake"),
+                getenv("DB_USER", "fake"),
+                getenv("DB_PASSWORD", "fake"),
                 Integer.parseInt(getenv("DB_POOL_SIZE", "10"))
         );
     }
 
     private static String getenv(String key, String defaultValue) {
-        String value = System.getenv(key);
+        String value = System.getenv(key); // gets the credentials from the local .env file
+        //!! THE FILE MUST BE ADDED TO THE ENVIROMENT VARIABLES FIELD IN THE SERVER CONFIGURATIONS TAB WITH THE ENV EXTENTION INSTALLED
         return value == null || value.isBlank() ? defaultValue : value;
     }
 }
